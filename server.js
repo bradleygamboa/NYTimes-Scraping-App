5edfcaddbb2ba40cae7c4fbc65a937a229f5b416
Bradley Gamboa <bradleygamboa@gmail.com>
2016-12-12 01:26:45 -0600

    heroku stuff

--- server.js
+++ server.js
@@ -4,19 +4,34 @@ var app = express();
 var mongoose = require('mongoose');
 
 /*Mongoose Connect*/
-var db = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL ||
-    'mongodb://heroku_xp4bh4sv:7tpu9f40ero5gbd5ear8mtnhj5@ds127998.mlab.com:27998/heroku_xp4bh4sv';
+var dbURI = 'mongodb://heroku_xp4bh4sv:7tpu9f40ero5gbd5ear8mtnhj5@ds127998.mlab.com:27998/heroku_xp4bh4sv';
 
-mongoose.connect(db, function(err){
-  if(err){
-    console.log(err);
-  } else {
-    console.log('mongoose connection is sucessful');
-  }
-});
+// mongoose.connect(db, function(err){
+//   if(err){
+//     console.log(err);
+//   } else {
+//     console.log('mongoose connection is sucessful');
+//   }
+// });
 
 app.use(express.static(__dirname + '/public'));
-var port = process.env.PORT || 3000;
+// var port = process.env.PORT || 3000;
+
+if (process.env.MONGODB_URI) {
+    mongoose.connect(process.env.MONGODB_URI);
+} else {
+    mongoose.connect(dbURI);
+}
+
+var db = mongoose.connection;
+
+db.on('error', function(err) {
+	console.log('Mongoose Error: ', err);
+});
+
+db.once('open', function() {
+	console.log('Mongoose connection succesful.');
+});
 
 var expressHandlebars = require('express-handlebars');
 app.engine('handlebars', expressHandlebars({
@@ -40,6 +55,6 @@ app.use('/save', routes);
 app.use('/delete', routes);
 
 
-app.listen(port, function() {
-    console.log("lisenting on port:" + port);
-});
+// app.listen(port, function() {
+//     console.log("lisenting on port:" + port);
+// });
